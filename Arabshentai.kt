c23c1be937b01bda5e5a27da5ec347f8cfd98aa2
Moaliii <drashdotcom@gmail.com>
2025-05-21 19:28:18 +0300

    Create Arabshentai.kt

---
+++ Arabshentai.kt
@@ -0,0 +1,9 @@
+package eu.kanade.aniyomi.animeextension.ar.arabshentai
+
+import eu.kanade.aniyomi.multisrc.videosource.VideoSource
+
+class animinice : VideoSource(
+    "arabshentai",
+    https://arabshentai.com
+    "ar"
+)
